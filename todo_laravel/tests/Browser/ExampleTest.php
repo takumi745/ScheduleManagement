956ffa86d4d4acf29103aeefaa8bb8458c25dc11
shinya <kms2wa8z@gmail.com>
2022-02-15 21:50:07 +0900

    2/15ブランチ切り替える前の状態

--- todo_laravel/tests/Browser/ExampleTest.php
+++ todo_laravel/tests/Browser/ExampleTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Browser;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Laravel\Dusk\Browser;
+/** use Laravel\Dusk\Browser; **/
+use Laravel\Dusk\Chrome;
 use Tests\DuskTestCase;
 
 class ExampleTest extends DuskTestCase
@@ -15,8 +17,15 @@ class ExampleTest extends DuskTestCase
      */
     public function testBasicExample()
     {
-        $this->browse(function (Browser $browser) {
+        /**$this->browse(function (Browser $browser) {
             $browser->visit('/test_todo');
+        });**/
+        $this->browse(function ($browser){
+            $browser->visit('/test_todo');
+                    /**->type('email', $user->email)
+                    ->type('password', 'password')
+                    ->press('Login')
+                    ->assertPathIs('/home');**/
         });
     }
 }
--- todo_laravel/tests/DuskTestCase.php
+++ todo_laravel/tests/DuskTestCase.php
@@ -20,7 +20,7 @@ abstract class DuskTestCase extends BaseTestCase
     public static function prepare()
     {
         if (! static::runningInSail()) {
-            static::startChromeDriver();
+            //static::startChromeDriver();
         }
     }
 
@@ -43,11 +43,15 @@ abstract class DuskTestCase extends BaseTestCase
             ]);
         })->all());
 
+        //return RemoteWebDriver::create(
+            //$_ENV['DUSK_DRIVER_URL'] ?? '\vendr/ralavel/dusk/bin',
+            //DesiredCapabilities::chrome()->setCapability(
+                //ChromeOptions::CAPABILITY, $options
+            //)
+        //);
+        //http://127.0.0.1:8000
         return RemoteWebDriver::create(
-            $_ENV['DUSK_DRIVER_URL'] ?? '\vendr/ralavel/dusk/bin',
-            DesiredCapabilities::chrome()->setCapability(
-                ChromeOptions::CAPABILITY, $options
-            )
+            'http://localhost:4444/wd/hub', DesiredCapabilities::phantomjs()
         );
     }
 
